62e736ae8ea970758f27a907914e6f0bb3378e45
gunnmgirl <48892214+gunnmgirl@users.noreply.github.com>
2020-07-22 11:51:27 +0200

    Adds routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import AllProducts from "./features/allProducts/components/AllProducts";
+import MyProducts from "./features/myProducts/components/MyProducts";
+import ProductDetails from "./features/productDetails/components/ProductDetails";
+import User from "./features/user/components/User";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/admin/:id" component={User}></Route>
+          <Route path="/admin" exact component={MyProducts}></Route>
+          <Route path="/:id" component={ProductDetails}></Route>
+          <Route path="/" component={AllProducts}></Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
